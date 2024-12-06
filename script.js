a5d0d91f10908ebb98f56ef5ae0b5b3b01bcb9d6
nimratahir142 <khanzaadi142@gmail.com>
2024-12-06 10:29:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function  myfunction() {
+    document.getElementById('ok').style.background = 'pink'
+    document.getElementById('ok').style.color = 'white'
+    document.getElementById('demo').style.fontFamily = 'cursive'
+    document.getElementById('demo').style.color = 'white'
+}
+window.alert('stop stop in the name of law')
